b20c10637a474616ab53c66dfb6a192385c74f64
佐藤樹 <satouitsuki@satoujunoMacBook-Pro.local>
2025-12-05 12:30:25 +0900

    12/5修正

--- app5.js
+++ app5.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static('public'));
+
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 
@@ -61,6 +63,28 @@ app.get("/janken", (req, res) => {
     total: total
   }
   res.render( 'janken', display );
+});
+
+app.get("/keiyo_add", (req, res) => {
+  let id = req.query.id;
+  let code = req.query.code;
+  let name = req.query.name;
+  let newdata = { id: id, code: code, name: name };
+  station.push( newdata );
+});
+
+let station = [
+  { id:1, code:"JE01", name:"東京駅"},
+  { id:2, code:"JE07", name:"舞浜駅"},
+  { id:3, code:"JE12", name:"新習志野駅"},
+  { id:4, code:"JE13", name:"幕張豊砂駅"},
+  { id:5, code:"JE14", name:"海浜幕張駅"},
+  { id:6, code:"JE05", name:"新浦安駅"},
+];
+
+app.get("/keiyo", (req, res) => {
+  // 本来ならここにDBとのやり取りが入る
+  res.render('db1', { data: station });
 });
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
